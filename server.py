99f8e6f908d5c344d13b13388e8799284da91231
Emma Dayton <emma.dayton85@gmail.com>
2019-03-14 13:02:33 -0700

    create connection to database to render emails on html, add jinja for loop to display emails on success page, update index to match name for post route

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, session, flash
+import re
 from mysqlconnection import connectToMySQL
 app = Flask(__name__)
-app.secret_key = '4a136bc896d8e3657d1799320bb2aa37'
+app.secret_key = '935d94dafc5f03c6567ae138c60cf105'
+email_regex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 
 @app.route('/')
@@ -9,10 +11,15 @@ def email_valid():
     session['title'] = 'Email Validation'
     return render_template('index.html')
 
-@app.route('/success')
+@app.route('/success', methods=["POST"])
 def success():
+    email = request.form['email']
+    print(email, '&&&&&&&&&&&&&&&&&&&&&&&&&&&')
     session['title'] = 'Success'
-    return render_template('success.html')
+    db = connectToMySQL('dojo_emails')
+    query = 'SELECT * FROM emails'
+    emails = db.query_db(query)
+    return render_template('success.html', emails=emails)
 
 
 if __name__ == "__main__":
